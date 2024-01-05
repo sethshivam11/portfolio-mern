b8bd9ff57261db33258104be19d498f3be288abe
blaisepascal287 <130232913+blaisepascal287@users.noreply.github.com>
2024-01-06 00:04:46 +0530

    Fix: Imageview.tsx file error

--- client/src/components/ImageView.tsx
+++ client/src/components/ImageView.tsx
@@ -1,11 +1,11 @@
-import React, { ReactElement } from "react";
-import logo from "../assets/logo.jpeg";
-const ImageView: React.FC<{ viewImage: boolean }> = ({ viewImage }) => {
-  return (
-    <div className="h-20 w-20">
-      <img src={logo} alt="" />
-    </div>
-  );
-};
+// import React, { ReactElement } from "react";
+// import logo from "../assets/logo.jpeg";
+// const ImageView: React.FC<{ viewImage: boolean }> = ({ viewImage }) => {
+//   return (
+//     <div className="h-20 w-20">
+//       <img src={logo} alt="" />
+//     </div>
+//   );
+// };
 
-export default ImageView;
+// export default ImageView;
